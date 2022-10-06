e3eaf56242381a31f722576236f64a94c841f2cf
jahongirakhmadjonov <115130799+jahongirakhmadjonov@users.noreply.github.com>
2022-10-06 09:17:56 +0500

    Add files via upload

---
+++ 1a22.py
@@ -0,0 +1,8 @@
+#22.	Aylana uzunligi l berilgan. Bu aylananing radiusini va aylana chegaralagan doiraning yuzasini hisoblash dasturi tuzilsin. =3,14 ga teng deb olinsin.
+import math
+pi=3.14
+l=int(input("Aylana uzunligini kiriting : "))
+r=l/2*pi
+s=pi*r**2
+print("Aylana radiusi : ",r)
+print("Doira yuzasi : ",s)
