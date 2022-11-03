9887001de9eb2f0ea685f8420c1dfed50499a563
Jahongir Ahmadjonov <115130799+jahongirakhmadjonov@users.noreply.github.com>
2022-11-03 10:26:11 +0500

    Add files via upload

---
+++ 2a22.py
@@ -0,0 +1,7 @@
+import math
+x=int(input("x ni kiriting: "))
+y=int(input("y ni kiriting: "))
+if x<0 and y>0:
+    print("Bu ikkinchi chorakda yotadi")
+else:
+    print("Bu ikkinchi chorakda yotmaydi")
