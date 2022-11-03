3d0b0f28d82b2c4d21bb6bc85b4815ed0278bc1e
Jahongir Ahmadjonov <115130799+jahongirakhmadjonov@users.noreply.github.com>
2022-11-03 10:21:09 +0500

    Add files via upload

---
+++ 1a222.py
@@ -0,0 +1,10 @@
+import math
+a=int(input("a ni kiriting: "))
+b=int(input("b ni kiriting: "))
+c=int(input("c ni kiriting: "))
+as1=a+b+c
+bs=b+c
+y=a+b+c
+print("AS :",as1)
+print("BS :",bs)
+print("Yig'indisi: ",y)
